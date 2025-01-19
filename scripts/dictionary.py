53416fb82b8b7afd62329e42ec5c2245303cb18d
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2025-01-20 05:32:54 +0800

    Add dictionary.py
    Operations about the JSON dictionary.
    
    * load(): Obtain the dictionary as an OrderedDict from a .json file.
    * print_yaml(): Output the corresponding test cases in the YAML format.
    
    If it runs as the main script, then the JSON data is printed with sorted
    keys. The keys, i.e. phrases, are sorted in ascending order by their code
    point in Unicode.

---
+++ scripts/dictionary.py
@@ -0,0 +1,51 @@
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2025 Bo-Cheng Jhan <school510587@yahoo.com.tw>
+# This file is covered by the GNU General Public License.
+# See the file LICENSE for more details.
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from collections import OrderedDict
+from operator import concat, countOf, ior
+import codecs
+import json
+import os
+import re
+import sys
+
+try: reduce
+except: from functools import reduce
+
+try: unichr
+except: unichr = chr
+
+def load(json_path):
+    with codecs.open(json_path, encoding="UTF-8") as json_file:
+        json_content = json_file.read()
+        data = json.JSONDecoder(object_pairs_hook=OrderedDict).decode(json_content)
+        for phrase, dot_pattern in data.items():
+            if len(phrase) != len(dot_pattern):
+                raise ValueError("Inconsistent {0} => {1}".format(phrase, dot_pattern))
+            for i, cells in enumerate(dot_pattern):
+                for j, cell in enumerate(cells.split("-")):
+                    if len(cell) < 1:
+                        raise ValueError("Empty cell in character {0} cell {1} of phrase {2}".format(i, j, phrase))
+                    if not re.match("^0|1?2?3?4?5?6?7?8?$", cell):
+                        raise ValueError("Format error in character {0} cell {1} of phrase {2}".format(i, j, phrase))
+        return data
+
+brl = lambda p: "".join(unichr(0x2800 | (0 if x == '0' else reduce(ior, (1 << (ord(d) - ord('1')) for d in x)))) for x in "-".join(p).split("-"))
+def print_yaml(data, testcase):
+    for p in sorted(data.keys()):
+        print('  - - "{0}"'.format(p), file=testcase)
+        print('    - "{0}"'.format(brl(data[p])), file=testcase)
+        print("    - outputPos:", "[0" + (", %d" * (len(p) - 1)) % tuple((countOf("-".join(data[p][:i]), "-") + 1 ) for i in range(1, len(p))) + "]", file=testcase)
+        print("      inputPos:", reduce(concat, ([i] * (countOf(data[p][i], "-") + 1) for i in range(len(p)))), file=testcase)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage:", sys.argv[0], "<path/to/zh-tw-dictionary.json>", file=sys.stderr)
+        print("Without format error, the stdout is the dictionary with sorted phrases.", file=sys.stderr)
+        exit()
+    data = load(sys.argv[1])
+    print(json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True))
